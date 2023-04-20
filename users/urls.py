e377811f384d98d78ce1cabcd63784d7d569de78
Jaswin Prakash K <jaswinprakash@gmail.com>
2023-04-20 15:00:31 +0530

    modified signup functionality

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ app_name = "users"
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, reverse
-
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.http.response import HttpResponseRedirect
+from django.contrib.auth.models import User
+
+from users.forms import UserForm
 
 
 def login(request):
@@ -22,12 +24,29 @@ def login(request):
                     "message": "Invalid username or password"
                 }
             return render(request, "users/login.html", context=context)
-    context = {
-        "title" : "Student Login"
-    }
-    return render(request, "users/login.html", context=context)
+    else:
+        context = {
+            "title" : "Student Login"
+        }
+        return render(request, "users/login.html", context=context)
 
 def logout(request):
     auth_logout(request)
     return HttpResponseRedirect(reverse("web:index"))
 
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+
+            User.objects.create_user(
+                
+            )
+    else:
+        form = UserForm()
+        context = {
+            "title" : "Sign Up",
+            "form" : form,
+        }
+        return render(request, "users/signup.html", context=context)
