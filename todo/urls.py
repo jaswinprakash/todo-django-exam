56c995db2ca065563c7e4a189e8e67e516cea0de
Jaswin Prakash K <jaswinprakash@gmail.com>
2023-04-20 10:15:18 +0530

    login page styiling

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("web.urls",namespace="web")),
+    path('users/',include("users.urls",namespace="users")),
 ]
